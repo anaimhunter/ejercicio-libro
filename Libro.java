a186ae277a3520ea7b1f4a2cd32aba9cdbc067dc
Ana M.Hunter <anaimhunter@hotmail.com>
2017-08-24 10:33:49 -0500

    Se agregaron dos variables de tipo cadena (titulo y autor), las cuales se les asigna la información en el constructor.

---
+++ Libro.java
@@ -0,0 +1,15 @@
+
+
+public class Libro
+{
+    private String titulo;
+    private String autor;
+    
+    public Libro(String tituloI, String autorI)
+    {
+        titulo=tituloI;
+        autor=autorI;
+    }
+  
+  
+}
