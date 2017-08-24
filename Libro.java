d5e7187b76a14248fbba0d0f82b6b6184d4454f9
Ana M.Hunter <anaimhunter@hotmail.com>
2017-08-24 11:00:24 -0500

    Se creo un método (dimeDetalles) el cual te muestra el título, el autor y el número de paginas

--- Libro.java
+++ Libro.java
@@ -4,11 +4,13 @@ public class Libro
 {
     private String titulo;
     private String autor;
+    private int paginas;
     
-    public Libro(String tituloI, String autorI)
+    public Libro(String tituloI, String autorI, int paginasI)
     {
         titulo=tituloI;
         autor=autorI;
+        paginas=paginasI;
     }
     
     public String dimeAutor()
@@ -21,5 +23,23 @@ public class Libro
         return titulo;
     }
     
+    public int dimePaginas()
+    {
+        return paginas;
+    }
     
+    public String dimeDetalles()
+    {
+        String cadDet = "";
+        cadDet = cadDet +"Titulo:";
+        cadDet = cadDet + titulo;
+        cadDet = cadDet +",";
+        cadDet = cadDet + "Autor:";
+        cadDet = cadDet + autor;
+        cadDet = cadDet +",";
+        cadDet = cadDet + "Paginas:";
+        cadDet = cadDet + paginas;
+        cadDet = cadDet +".";
+        return cadDet;
+    }
 }
