6bc0ef5e4634a45ced179899d3e8978931772dc7
Ana M.Hunter <anaimhunter@hotmail.com>
2017-08-24 18:04:19 -0500

    Se agrego una variable de instacia (numeroDeReferencia) la cual se inicializa en el constructor, se crean dos métodos (cambiaNumRef y dimeNumRef). El método dimeDetalles es modificado para que también se muestre el numero de referencia.

--- Libro.java
+++ Libro.java
@@ -5,12 +5,14 @@ public class Libro
     private String titulo;
     private String autor;
     private int paginas;
+    private String numeroDeReferencia;
     
     public Libro(String tituloI, String autorI, int paginasI)
     {
         titulo=tituloI;
         autor=autorI;
         paginas=paginasI;
+        numeroDeReferencia = "";
     }
     
     public String dimeAutor()
@@ -39,7 +41,30 @@ public class Libro
         cadDet = cadDet +",";
         cadDet = cadDet + "Paginas:";
         cadDet = cadDet + paginas;
+        cadDet = cadDet +",";
+        cadDet = cadDet + "Numero de Referencia:";
+        if(numeroDeReferencia.length()!=0)
+        {
+           cadDet = cadDet + numeroDeReferencia; 
+        }
+        else
+        {
+            cadDet = cadDet + "ZZZ";
+        }
         cadDet = cadDet +".";
         return cadDet;
+    }
+    
+    public void cambiaNumRef(String nuevaRef)
+    {
+        if(numeroDeReferencia.length()>2)
+        {
+            numeroDeReferencia = nuevaRef;
+        }
+    }
+    
+    public String dimeNumRef()
+    {
+        return numeroDeReferencia;
     }
 }
